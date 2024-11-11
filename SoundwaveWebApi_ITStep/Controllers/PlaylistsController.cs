e33dd64e1ab1af61dcf406eb4320343ea7109786
nitesw <111660360+nitesw@users.noreply.github.com>
2024-11-11 20:34:01 +0100

    Update PlaylistsController.cs

--- SoundwaveWebApi_ITStep/Controllers/PlaylistsController.cs
+++ SoundwaveWebApi_ITStep/Controllers/PlaylistsController.cs
@@ -22,21 +22,21 @@ namespace SoundwaveWebApi_ITStep.Controllers
             this.playlistService = playlistService;
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpGet("all")]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await playlistService.GetAll());
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpGet("getPlaylist")]
         public async Task<IActionResult> Get(int id)
         {
             return Ok(await playlistService.Get(id));
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpPost("create")]
         public async Task<IActionResult> Create([FromForm] CreatePlaylistDto model)
         {
@@ -45,7 +45,7 @@ namespace SoundwaveWebApi_ITStep.Controllers
             return Ok();
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpPut("edit")]
         public async Task<IActionResult> Edit([FromForm] EditPlaylistDto model)
         {
@@ -54,7 +54,7 @@ namespace SoundwaveWebApi_ITStep.Controllers
             return Ok();
         }
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpDelete("delete")]
         public async Task<IActionResult> Delete(int id)
         {
